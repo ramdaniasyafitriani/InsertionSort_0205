785844b3c2b74487f911a431b1b7bc8908ff65d9
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-03-21 21:03:48 +0700

    make procedure input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,24 @@ using namespace std;
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
 
+void input()
+{ // Procedure input
+    while (true) 
+    {
+        cout << "Masukkan jumlah data pada array : "; // Membuat inputan jumlah element array 
+        cin >> n; // memanggil variable inputan n
+
+        if (n <= 20)
+        { // membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else 
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl; // membuat jarak per baris program
+    cout << "======================" << endl; // membuat tampilan susunan data pada array 
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+}
