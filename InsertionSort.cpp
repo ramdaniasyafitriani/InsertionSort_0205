d1066e5130f0ab7def5e4954c4b58819b00f51e6
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-03-21 21:16:39 +0700

    make procedure insertionsort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -31,3 +31,23 @@ void input()
         cin >> arr[i]; // menyimpan data n kedalam array arr
     }
 }
+
+void insertionSort()
+{ // Procedure Insertionsort
+    int temp; // Membuat variable data temporer atau penyimpanan sementra
+    int j, i; // Membuat variable j sebagai penanda
+    
+    for (i = 1; i <= n - 1; i++)
+    { // step 1
+        temp = arr[i]; // step 2
+
+        j = i - 1; // step 3
+
+        while (j >= 0 && arr [j] > temp) // step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j--; // step 4b
+        }
+        arr[j + 1] = temp; // step 5
+    }
+}
